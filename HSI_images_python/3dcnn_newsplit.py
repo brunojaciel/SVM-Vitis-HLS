60a34791edf01798c518d2cb137922db54a65ed9
Felipe Viel <viel@univali.br>
2021-11-11 16:09:48 -0300

    Add images and python script to open

---
+++ HSI_images_python/3dcnn_newsplit.py
@@ -0,0 +1,43 @@
+!pip install spectral
+
+# Commented out IPython magic to ensure Python compatibility.
+import keras
+from keras.layers import Conv2D, Conv3D, Flatten, Dense, Reshape, BatchNormalization
+from keras.layers import Dropout, Input
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, cohen_kappa_score
+
+from operator import truediv
+
+from plotly.offline import init_notebook_mode
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import os
+import spectral
+
+
+## GLOBAL VARIABLES
+dataset = 'IP'
+
+def loadData(name):
+    data_path = os.path.join(os.getcwd(),'data')
+    if name == 'IP':
+        data = sio.loadmat(os.path.join(data_path, 'Indian_pines_corrected.mat'))['indian_pines_corrected']
+        labels = sio.loadmat(os.path.join(data_path, 'Indian_pines_gt.mat'))['indian_pines_gt']
+    elif name == 'SA':
+        data = sio.loadmat(os.path.join(data_path, 'Salinas_corrected.mat'))['salinas_corrected']
+        labels = sio.loadmat(os.path.join(data_path, 'Salinas_gt.mat'))['salinas_gt']
+    elif name == 'PU':
+        data = sio.loadmat(os.path.join(data_path, 'PaviaU.mat'))['paviaU']
+        labels = sio.loadmat(os.path.join(data_path, 'PaviaU_gt.mat'))['paviaU_gt']
+    
+    return data, labels
+
